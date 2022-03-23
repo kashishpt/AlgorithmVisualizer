5aa33b2f46de1717e206bf288dc98d8c8fad414b
kashishpt <82962026+kashishpt@users.noreply.github.com>
2022-03-22 20:35:19 -0400

    Delete src directory

--- src/components/Start.js
+++
@@ -1,9 +0,0 @@
-import {dijkstras} from '../algorithms/Dijkstras'
-
-function Start(props) {
-  return (
-    <button onClick={() => dijkstras(props.height, props.length)}>Start</button>
-  )
-}
-
-export default Start
